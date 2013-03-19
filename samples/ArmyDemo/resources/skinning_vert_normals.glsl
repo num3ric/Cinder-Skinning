32ba836ec70b001f5bcb282bf7005f71150dee6c
num3ric <eric.renaud.houde@gmail.com>
2013-03-19 18:44:36 -0400

    Kinect project compiles and includes the Kinect-SDK block.

---
+++ samples/ArmyDemo/resources/skinning_vert_normals.glsl
@@ -0,0 +1,38 @@
+const int MAXBONES = 92;
+
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec2 texcoord;
+attribute vec4 boneWeights;
+attribute vec4 boneIndices;
+
+uniform bool isAnimated;
+uniform mat4 boneMatrices[MAXBONES];
+uniform mat4 invTransposeMatrices[MAXBONES];
+
+varying vec2 Tc;
+varying vec3 N, L, V;
+
+void main()
+{	
+	vec4 pos = vec4(position, 1.0);
+	vec4 norm = vec4(normal, 1.0);
+	if( isAnimated ) {
+		pos =	boneMatrices[int(boneIndices.x)] * pos * boneWeights.x +
+		boneMatrices[int(boneIndices.y)] * pos * boneWeights.y +
+		boneMatrices[int(boneIndices.z)] * pos * boneWeights.z +
+		boneMatrices[int(boneIndices.w)] * pos * boneWeights.w ;
+		
+		norm =  invTransposeMatrices[int(boneIndices.x)] * norm * boneWeights.x +
+		invTransposeMatrices[int(boneIndices.y)] * norm * boneWeights.y +
+		invTransposeMatrices[int(boneIndices.z)] * norm * boneWeights.z +
+		invTransposeMatrices[int(boneIndices.w)] * norm * boneWeights.w ;
+		pos.w = 1.0;
+		norm.w = 0.0;
+	}
+	V = (gl_ModelViewMatrix * pos).xyz;
+	N = normalize(gl_NormalMatrix * norm.xyz);
+	L = normalize(vec3(gl_LightSource[0].position));
+	Tc = texcoord;
+	gl_Position = gl_ModelViewProjectionMatrix * pos;
+}
--- samples/SeymourDemo/include/Resources.h
+++ samples/SeymourDemo/include/Resources.h
@@ -3,4 +3,3 @@
 
 #define RES_SKINNING_VERT			CINDER_RESOURCE( ../../../resources/, skinning_vert_normals.glsl, 128, GLSL  )
 #define RES_SKINNING_FRAG			CINDER_RESOURCE( ../../../resources/, skinning_frag_normals.glsl, 129, GLSL  )
-
