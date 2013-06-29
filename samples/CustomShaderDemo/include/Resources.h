6176b0b57198c5aaff5af155df19e4aa417174a3
num3ric <eric.renaud.houde@gmail.com>
2013-06-29 22:27:04 +0200

    Fixed resource inclusion problem upon project creation.

--- samples/CustomShaderDemo/include/Resources.h
+++ samples/CustomShaderDemo/include/Resources.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "cinder/CinderResources.h"
 
-//TODO: remove the need for always having these two to compile the block
 #define RES_SKINNING_VERT			CINDER_RESOURCE( ../../../resources/, skinning_vert_normals.glsl, 128, GLSL  )
 #define RES_SKINNING_FRAG			CINDER_RESOURCE( ../../../resources/, skinning_frag_normals.glsl, 129, GLSL  )
 
