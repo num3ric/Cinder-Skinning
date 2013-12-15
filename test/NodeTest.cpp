8a9defd8fa85b8bad774d34fb7fee9c3d962cf76
num3ric <eric.renaud.houde@gmail.com>
2013-12-15 02:37:21 -0500

    First functional boost test trial.

--- test/NodeTest.cpp
+++ test/NodeTest.cpp
@@ -9,34 +9,67 @@
 
 #include <boost/test/unit_test.hpp>
 #include "Node.h"
+#include "Skeleton.h"
+#include "cinder/Vector.h"
+#include "cinder/Quaternion.h"
+#include <vector>
 
 using namespace ci;
 using namespace std;
+using namespace model;
 
 typedef std::chrono::high_resolution_clock Clock;
 
 
 BOOST_AUTO_TEST_CASE(SimpleTestInMainTestingModule)
 {
-	BOOST_CHECK(4 == 2);
+	BOOST_CHECK( true );
 	
 }
 
 
 BOOST_AUTO_TEST_CASE(PerformanceTest)
 {
-	{
-		cout << "Performance test." << endl;
-		auto start = Clock::now();
-		
-		int test = 0;
-		for( long i=0; i<10000000; ++i ) {
-			test = test + (i % 23423 );
+	//Make a very large linked list of nodes
+	
+	SkeletonRef skeleton = Skeleton::create();
+//	skeleton->
+	
+	vector<NodeRef> nodes;
+	int num = 10000;
+	for( int i=0; i<num; ++i ) {
+		if( i==0) {
+			nodes.push_back( make_shared<Node>( Vec3f::zero(), Quatf(), Vec3f::one(), "root", nullptr, i ) );
+		} else {
+			NodeRef child = make_shared<Node>( Vec3f::zero(), Quatf(), Vec3f::one(), "", nodes.back(), i );
+			nodes.back()->addChild( child );
+			nodes.push_back( child );
 		}
-		auto end = Clock::now();
+	}
+	
+	//Verify that all nodes are properly linked
+	for( auto& node : nodes ) {
+		if( node->getLevel() != num - 1 ) {
+			BOOST_CHECK( node->getNumChildren() == 1 );
+		}
 		
-		auto nsdur = chrono::duration_cast<chrono::nanoseconds>( end - start );
-		auto msdur = chrono::duration_cast<chrono::milliseconds>( end - start );
-		cout << nsdur.count() << " ns,  " << msdur.count() << "ms" << endl;
+		if( node->getName() != "root" ) {
+			BOOST_CHECK( node->getParent().lock() != nullptr );
+		}
+	}
+	for( int r=0; r<100; ++r ) {
+		nodes.front()->setRelativeRotation( Quatf( Vec3f(0.0f,1.0f,0.0f), M_PI ) );
+		{
+			cout << "Animate & update 10000 nodes: ";
+			auto start = Clock::now();
+			for( auto& node : nodes ) {
+				node->animate(0.0f);
+			}
+			auto end = Clock::now();
+			
+			auto nsdur = chrono::duration_cast<chrono::nanoseconds>( end - start );
+			auto msdur = chrono::duration_cast<chrono::milliseconds>( end - start );
+			cout << nsdur.count() << " ns,  " << msdur.count() << "ms" << endl;
+		}
 	}
 }
